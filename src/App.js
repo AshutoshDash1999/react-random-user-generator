ce5e029befa078b51f183dfe2d8021f39c90a1c3
AshutoshDash1999 <dashashutosh1999@gmail.com>
2022-04-19 14:31:26 +0530

    first commit, generate random user on load

--- src/App.js
+++ src/App.js
@@ -1,22 +1,125 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
+import axios from "axios";
+import { useEffect } from "react";
+import ProfileCard from "./ProfileCard";
 
 function App() {
+  const [genderFilter, setGenderFilter] = useState("");
+  const [name] = useState({
+    title: "",
+    first: "",
+    last: "",
+  });
+
+  const [contact] = useState({
+    email: "",
+    phone: "",
+  });
+
+  const [dob] = useState({
+    age: "",
+    date: "",
+  });
+
+  const [location] = useState({
+    city: "",
+    state: "",
+    country: "",
+    postcode: "",
+  });
+
+  const [picture, setPicture] = useState("");
+
+  const [login] = useState({
+    username: "",
+    password: "",
+  });
+  const [ascentColor, setAscentColor] = useState("");
+
+  const apiURL = "https://randomuser.me/api/";
+
+  const apiCall = (url) => {
+    axios
+      .get(url)
+      .then((response) => {
+        const userData = response.data.results[0];
+        // console.log(userData);
+        name.title = userData.name.title;
+        name.first = userData.name.first;
+        name.last = userData.name.last;
+
+        contact.email = userData.email;
+        contact.phone = userData.phone;
+
+        dob.age = userData.dob.age;
+        dob.date = new Date(userData.dob.date).toDateString();
+
+        location.city = userData.location.city;
+        location.state = userData.location.state;
+        location.country = userData.location.country;
+        location.postcode = userData.location.postcode;
+
+        login.username = userData.login.username;
+        login.password = userData.login.password;
+
+        setPicture(userData.picture.large);
+      })
+      .catch((error) => {
+        alert("Something went wrong");
+        console.log(error);
+      });
+  };
+
+  const colorList = [
+    "hover:bg-red-700",
+    "hover:bg-orange-700",
+    "hover:bg-amber-700",
+    "hover:bg-yellow-700",
+    "hover:bg-lime-700",
+    "hover:bg-green-700",
+    "hover:bg-emerald-700",
+    "hover:bg-teal-700",
+    "hover:bg-cyan-700",
+    "hover:bg-violet-700",
+    "hover:bg-purple-700",
+    "hover:bg-fuchsia-700",
+    "hover:bg-pink-700",
+    "hover:bg-rose-700",
+    "hover:bg-indigo-700",
+    "hover:bg-blue-700",
+    "hover:bg-cyan-700",
+  ];
+
+  useEffect(() => {
+    apiCall(apiURL);
+    setAscentColor(colorList[Math.floor(Math.random() * colorList.length)]);
+  }, []);
+
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        {name.first ? (
+          <ProfileCard
+            picture={picture}
+            title={name.title}
+            firstName={name.first}
+            lastName={name.last}
+            email={contact.email}
+            phone={contact.phone}
+            dob={dob.date}
+            age={dob.age}
+            city={location.city}
+            state={location.state}
+            country={location.country}
+            postcode={location.postcode}
+            username={login.username}
+            password={login.password}
+            color={ascentColor}
+          />
+        ) : (
+          <h1>Waiting for data....</h1>
+        )}
       </header>
     </div>
   );
---
+++ src/ProfileCard.js
@@ -0,0 +1,74 @@
+import React from "react";
+import { useState } from "react";
+import {
+  FaLocationArrow,
+  FaUser,
+  FaCalendarAlt,
+  FaPhoneAlt,
+} from "react-icons/fa";
+import { ImKey } from "react-icons/im";
+import { MdEmail } from "react-icons/md";
+
+function ProfileCard(props) {
+  const [profileData, setProfileData] = useState(
+    `${props.title}. ${props.firstName} ${props.lastName}`
+  );
+
+  const showName = () => {
+    setProfileData(`${props.title}. ${props.firstName} ${props.lastName}`);
+  };
+  const showMail = () => {
+    setProfileData(`${props.email}`);
+  };
+  const showPhone = () => {
+    setProfileData(`${props.phone}`);
+  };
+  const showDOB = () => {
+    setProfileData(`DOB: ${props.dob}; Age: ${props.age}`);
+  };
+  const showAddress = () => {
+    setProfileData(
+      `${props.city}, ${props.state}, ${props.country} - ${props.postcode}`
+    );
+  };
+  const showIdPassword = () => {
+    setProfileData(`UserID: ${props.username}; Password: ${props.password}`);
+  };
+
+  const buttonStyle = `user-info-btn p-4 mx-4 my-8 rounded-md whitespace-normal ${props.color} transition ease-in-out shadow-2xl`;
+
+  console.log(`${props.color}`);
+
+  return (
+    <div
+      className={`random-user-box p-8 rounded-xl w-1/2 max-h-fit transition ease-in-out duration-300`}
+    >
+      <div className="mx-auto flex justify-center">
+        <img className="rounded-full" src={props.picture} alt="" />
+      </div>
+      <div>{profileData}</div>
+      <div className="flex justify-center items-center flex-wrap	">
+        <div onMouseEnter={showName} className={buttonStyle}>
+          <FaUser />
+        </div>
+        <div onMouseEnter={showMail} className={buttonStyle}>
+          <MdEmail />
+        </div>
+        <div onMouseEnter={showPhone} className={buttonStyle}>
+          <FaPhoneAlt />
+        </div>
+        <div onMouseEnter={showDOB} className={buttonStyle}>
+          <FaCalendarAlt />
+        </div>
+        <div onMouseEnter={showAddress} className={buttonStyle}>
+          <FaLocationArrow />
+        </div>
+        <div onMouseEnter={showIdPassword} className={buttonStyle}>
+          <ImKey />
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default ProfileCard;
